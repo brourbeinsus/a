b57ee1621a014ca8c35aa08e36199bf101056f46
brourbeinsus <79854224+brourbeinsus@users.noreply.github.com>
2022-05-31 13:59:01 -0400

    Update bro.js

--- bro.js
+++ bro.js
@@ -4,7 +4,7 @@
 	} else {
 		var s = document.createElement('script');
 	    s.type = 'text/css';
-	    s.src = 'https://raw.githubusercontent.com/brourbeinsus/a/main/thing.css';
+	    s.src = 'https://brourbeinsus.github.io/a/thing.css';
 	    document.body.appendChild(s);
 	}
 })();
